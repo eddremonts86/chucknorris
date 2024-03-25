c48628fbbfc5b0df9ffeec1688c5c8f52f18caa7
Eduardo Valdes Inerarte <eddremonts86@gmail.com>
2024-03-25 12:29:00 +0100

    refactor(global): fix with on app

--- code-new-implementation/src/components/FormWrapper.vue
+++ code-new-implementation/src/components/FormWrapper.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <div class="flex">
-      <div class="flex-col w-1/2">
+    <div class="flex flex-col md:flex-row">
+      <div class="flex-col w-full md:w-1/2">
         <form @submit.prevent="getRandom()" class="bg-gray-100 p-6 rounded-lg m-2 dark:bg-gray-800">
           <h2 class="text-xl font-semibold text-gray-800 dark:text-white mb-3">
             Generate random jokes
@@ -17,17 +17,19 @@
           />
           <cr-btn class="block my-5" type="submit" text="Get a random Joke" />
           <br />
-          <b class="dark:text-stone-100"
-            >Press <span style="color: #229392">'Ctrl + Y'</span> for new joke</b
-          >
+          <b class="dark:text-stone-100">
+            Press
+            <span style="color: #229392">'Ctrl + Y'</span>
+            for new joke
+          </b>
         </form>
         <form @submit.prevent="create()" class="bg-gray-100 p-6 rounded-lg m-2 dark:bg-gray-800">
           <h2 class="text-xl font-semibold text-gray-800 dark:text-white mb-3">
             Create your own jokes
           </h2>
 
-          <label for="createJokes" class="pb-2 dark:text-white"
-            >Write a joke:
+          <label for="createJokes" class="pb-2 dark:text-white">
+            Write a joke:
             <textarea
               class="block w-full rounded-xl px-3 py-4 dark:text-black"
               name="createJokes"
@@ -38,7 +40,7 @@
           <cr-btn type="submit" text="Create" class="block my-5" />
         </form>
       </div>
-      <div class="flex-col w-1/2" v-if="joke">
+      <div class="flex-col w-full md:w-1/2 justify-center items-center" v-if="joke">
         <div class="bg-gray-100 p-6 rounded-lg m-2 dark:bg-gray-700">
           <h2 class="text-xl font-semibold text-gray-800 dark:text-white">New joke</h2>
           <joke-card :key="joke.id" :joke="joke" @delete="deleteJokeById" />
@@ -49,66 +51,66 @@
 </template>
 
 <script>
-import FormCategorySelector from '@/components/FormCategorySelector.vue';
-import JokeCard from '@/components/globals/JokeCard.vue';
-import { mapActions, mapState } from 'vuex';
+  import FormCategorySelector from '@/components/FormCategorySelector.vue'
+  import JokeCard from '@/components/globals/JokeCard.vue'
+  import { mapActions, mapState } from 'vuex'
 
-export default {
-  components: {
-    FormCategorySelector,
-    JokeCard,
-  },
-  props: {
-    categories: {
-      type: Array,
-      required: true,
+  export default {
+    components: {
+      FormCategorySelector,
+      JokeCard,
     },
-  },
-  data() {
-    return {
-      jokeContent: '',
-      selectedCategory: '',
-    };
-  },
-  computed: {
-    ...mapState(['jokes', 'joke']),
-  },
-  methods: {
-    ...mapActions(['fetchJokeByCategory', 'fetchRandomJoke', 'updateJokes', 'updateJoke']),
-    create() {
-      if (!this.jokeContent) {
-        return;
-      }
-      const joke = { value: this.jokeContent, id: Date.now().toString() };
-      this.updateJoke(joke);
-      this.updateJokes([joke, ...this.jokes]);
-      this.jokeContent = '';
+    props: {
+      categories: {
+        type: Array,
+        required: true,
+      },
     },
-    async getRandom() {
-      if (this.selectedCategory) {
-        await this.fetchJokeByCategory(this.selectedCategory);
-      } else {
-        await this.fetchRandomJoke();
-      }
-      this.updateJokes([this.joke, ...this.jokes]);
-    },
-    async getRandomByKeyCombination(event) {
-      if (!event.ctrlKey) {
-        return;
-      }
-      if (event.code === 'KeyY') {
-        event.preventDefault();
-        await this.fetchRandomJoke();
-        this.updateJokes([this.joke, ...this.jokes]);
+    data() {
+      return {
+        jokeContent: '',
+        selectedCategory: '',
       }
     },
-  },
+    computed: {
+      ...mapState(['jokes', 'joke']),
+    },
+    methods: {
+      ...mapActions(['fetchJokeByCategory', 'fetchRandomJoke', 'updateJokes', 'updateJoke']),
+      create() {
+        if (!this.jokeContent) {
+          return
+        }
+        const joke = { value: this.jokeContent, id: Date.now().toString() }
+        this.updateJoke(joke)
+        this.updateJokes([joke, ...this.jokes])
+        this.jokeContent = ''
+      },
+      async getRandom() {
+        if (this.selectedCategory) {
+          await this.fetchJokeByCategory(this.selectedCategory)
+        } else {
+          await this.fetchRandomJoke()
+        }
+        this.updateJokes([this.joke, ...this.jokes])
+      },
+      async getRandomByKeyCombination(event) {
+        if (!event.ctrlKey) {
+          return
+        }
+        if (event.code === 'KeyY') {
+          event.preventDefault()
+          await this.fetchRandomJoke()
+          this.updateJokes([this.joke, ...this.jokes])
+        }
+      },
+    },
 
-  mounted() {
-    window.addEventListener('keypress', this.getRandomByKeyCombination);
-  },
-  beforeDestroy() {
-    window.removeEventListener('keypress', this.getRandomByKeyCombination);
-  },
-};
+    mounted() {
+      window.addEventListener('keypress', this.getRandomByKeyCombination)
+    },
+    beforeDestroy() {
+      window.removeEventListener('keypress', this.getRandomByKeyCombination)
+    },
+  }
 </script>
--- code-new-implementation/src/layout/BaseLayout.vue
+++ code-new-implementation/src/layout/BaseLayout.vue
@@ -4,18 +4,22 @@
       <nav-bar />
     </header>
 
-    <main class="bg-white dark:bg-slate-900 min-h-screen max-h-screen overflow-auto pb-16">
-      <slot />
+    <main
+      class="bg-white dark:bg-slate-900 min-h-screen max-h-screen overflow-auto pb-16 flex justify-center"
+    >
+      <div class="max-w-screen-3xl">
+        <slot />
+      </div>
     </main>
   </div>
 </template>
 <script>
-import NavBar from '@/components/globals/NavBar.vue';
+  import NavBar from '@/components/globals/NavBar.vue'
 
-export default {
-  name: 'BaseLayout',
-  components: {
-    NavBar,
-  },
-};
+  export default {
+    name: 'BaseLayout',
+    components: {
+      NavBar,
+    },
+  }
 </script>
